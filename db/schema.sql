daa937be74271fd2f0f6fec69075756432fb2cf2
skouch <skouchy24@gmail.com>
2023-04-17 00:03:48 -0600

    schema optimized with DELETE, mysql responding to schema

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE `roles` (
   `title` VARCHAR(30) NOT NULL,
   `salary` DECIMAL NOT NULL,
   `department_id` INT NOT NULL,
-  FOREIGN KEY (`department_id`) REFERENCES `departments`(`id`)
+  CONSTRAINT fk_department FOREIGN KEY (`department_id`) REFERENCES `departments`(`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `employees` (
@@ -25,6 +25,6 @@ CREATE TABLE `employees` (
   `last_name` VARCHAR(30) NOT NULL,
   `role_id` INT NOT NULL,
   `manager_id` INT, -- can be null in the case: role = manager, in which the manager would not report to another
-  FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`),
-  FOREIGN KEY (`manager_id`) REFERENCES `employees`(`id`)
+  CONSTRAINT fk_role FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`) ON DELETE CASCADE,
+  CONSTRAINT fk_manager FOREIGN KEY (`manager_id`) REFERENCES `employees`(`id`) ON DELETE CASCADE
 );
