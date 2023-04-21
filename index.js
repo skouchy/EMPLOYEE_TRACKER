ec905c8dd555eb8e5379194a8ec6f5a01c05545a
skouch <skouchy24@gmail.com>
2023-04-21 16:16:13 -0700

    Update Employee Role SUCCESS

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function mainMenu() {
     if (firstChoice === 'Update employee role') {
       updateRole();
     }
-    
+
 
   }
   )
@@ -73,6 +73,79 @@ function getEmployees() {
     mainMenu();
   })
 };
+
+function updateRole() {
+  db.query('SELECT employees.id, employees.first_name, employees.last_name FROM employees', (err, res) => {
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'selectEmployee',
+        message: 'Select Employee to Update',
+        choices: () => {
+          const employeeList = [];
+          res.forEach((employee) => {
+            const name = (employee.first_name + ' ' + employee.last_name);
+            const value = employee.id;
+            employeeList.push({ name, value });
+          })
+          return employeeList;
+        }
+      }
+    ])
+      .then((choiceEmployee) => {
+        db.query('SELECT roles.id, roles.title FROM roles', (err, rolesRes) => {
+
+          inquirer.prompt([
+            {
+              type: 'list',
+              name: 'selectRole',
+              message: 'What would you like to change their role to?',
+              choices: () => {
+                const roleList = [];
+                rolesRes.forEach((role) => {
+                  const name = role.title;
+                  const value = role.id;
+                  roleList.push({ name, value })
+                  // console.log(`DIS DA ROLE DATA RETURNED APPPAAAARRREEEENNTTLYYY: `, role);
+                })
+                // console.log(`rooolll  LEEEEEEST: `, roleList);
+                return roleList;
+              }
+            }
+          ])
+            .then((choiceRole) => {
+              for (let i = 0; i < rolesRes.length; i++) {
+                if (choiceRole.selectRole === (rolesRes[i].id)) {
+                  const sql = 'UPDATE employees SET ? WHERE ?';
+                  const params = [({ role_id: choiceRole.selectRole }), ({ id: choiceEmployee.selectEmployee })];
+                  db.query(sql, params, (err, row) => {
+                    if (err) {
+                      console.log(err);
+                      return;
+                    }
+                    console.log('Employee role updated :)');
+
+                  })
+                }
+              }
+              const sql = 'SELECT * FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id';
+              //   const params = [choiceEmployee.id];
+              db.query(sql, (err, resUpdated) => {
+                if(err){
+                  console.log(err);
+                }
+                console.log({ message: 'Here is the updated Employees Table' });
+                console.table(resUpdated);
+                mainMenu();
+                })
+              })
+            })
+        })
+      })
+  }
+
+
 
 
 // db.get('/employees', (req, res) => {
